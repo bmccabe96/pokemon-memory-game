08eac522fdd114adc66ca60bf2f3ab324bb4b314
bmccabe96 <brianmccabe96@gmail.com>
2022-04-04 20:39:56 -0400

    Added win message and also up'd cards from 12 to 28

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer"
 import Main from "./components/Main";
 
-//TESTING, THIS WILL BE IN MAIN LATER...
-import { Card } from "./components/pokemon/Card";
-import { Score } from "./components/Score";
 
 
 function App() {
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,56 @@
 import React, { useState, useEffect} from "react";
 import PokemonList from "./pokemon/PokemonList";
 import Lose from "./Lose";
+import Win from "./Win";
 import { Score } from "./Score";
 import { shuffle, sleep } from "../utils";
 
 const Main = () => {
 
+  const POKEMON_COUNT = 28;
   const [pokemons, setPokemons] = useState([]);
   const [selectedPokemons, setSelectedPokemons] = useState([]);
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
+  const [gameWon, setGameWon] = useState(false);
 
 
   useEffect(()  => {
     const loadCards = async () => {
-      setPokemons(await fetchPokemons(12));
+      setPokemons(await fetchPokemons(POKEMON_COUNT));
     }
     loadCards();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (selectedPokemons.length === POKEMON_COUNT) {
+        setBestScore(currentScore);
+        setGameWon(true);
+        console.log("waiting...");
+        await sleep(2000);
+        setGameWon(false);
+        setCurrentScore(0);
+        setSelectedPokemons([]);
+      }
+    })();
+    
+  }, [selectedPokemons, currentScore])
+
+  const fetchPokemons = async (n) => {
+    let pokemons = [];
+    for (let i=1; i<=n; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+      const response = await fetch(url);
+      const pokemon = await response.json();
+      const id = pokemon.id;
+      const name = pokemon.name;
+      const image = pokemon.sprites.front_default;
+      pokemons.push({ id, name, image, });
+    }
+    return shuffle(pokemons);
+  };
 
   const handleClick = async (id, name) => {
     let alreadyClicked = false;
@@ -33,7 +65,6 @@ const Main = () => {
         setBestScore(currentScore);
       }
       setGameOver(true);
-      console.log("waitiing...");
       await sleep(2000);
       setGameOver(false);
       setCurrentScore(0);
@@ -42,25 +73,12 @@ const Main = () => {
       const newSelectedPokemons = [...selectedPokemons, {'id': id, 'name': name}];
       setSelectedPokemons(newSelectedPokemons);
       setCurrentScore(currentScore + 1);
+      
     }
     const copyOfPokemons = shuffle([...pokemons]);
     setPokemons(copyOfPokemons);
 
-  }
-
-  const fetchPokemons = async (n) => {
-    let pokemons = [];
-    for (let i=1; i<=n; i++) {
-      const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
-      const response = await fetch(url);
-      const pokemon = await response.json();
-      const id = pokemon.id;
-      const name = pokemon.name;
-      const image = pokemon.sprites.front_default;
-      pokemons.push({ id, name, image, });
-    }
-    return shuffle(pokemons);
-  }
+  };
 
   return (
     <div className="main-content">
@@ -70,6 +88,7 @@ const Main = () => {
       </div>
       <PokemonList isGameOver={gameOver} pokemons={pokemons} handleClick={handleClick}/>
       {gameOver ? <Lose score={currentScore}/> : null}
+      {gameWon ? <Win /> : null}
     </div>
   )
 }
